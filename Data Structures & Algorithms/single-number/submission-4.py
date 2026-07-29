374f1c65468bb4bd689c2c9d4bac53fb3c34faea
Ammar <amjadammar786786@gmail.com>
2026-07-29 20:24:35 +0530

    Add: single-number - submission-4

---
+++ Data Structures & Algorithms/single-number/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # hash_set = set()
+
+        # for num in nums:
+        #     if num not in hash_set:
+        #         hash_set.add(num)
+        #     else:
+        #         hash_set.remove(num)
+
+        # return hash_set.pop()
+
+        r = 0
+        for n in nums:
+            r ^= n
+
+        return r 
+        
